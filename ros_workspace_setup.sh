99a8a3f7268e442fe1ff62a33deb589ed1e3750a
peterdavidfagan <peterdavidfagan@gmail.com>
2024-04-01 11:42:27 +0100

    migrate to humble

--- ros_workspace_setup.sh
+++ ros_workspace_setup.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
+set -e
 
 # install libfranka
-sudo apt update && apt upgrade -y
-sudo apt remove "*libfranka*" -y
+sudo apt update && apt upgrade -y || true
+sudo apt remove "*libfranka*" -y || true
 sudo apt install -y  build-essential cmake git libpoco-dev libeigen3-dev
 cd libfranka 
+if [ -d "./build" ]; then
+	rm -rf build
+fi
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF ..
 cmake --build .
 cpack -G DEB
 sudo dpkg -i libfranka*.deb
+cd ../..
 
 # source iron installation and install middleware
-source /opt/ros/iron/setup.bash
+source /opt/ros/humble/setup.bash
 sudo apt install ros-$ROS_DISTRO-rmw-cyclonedds-cpp -y
 
 # install moveit dependencies
-cd ./src/motion_planning
-for repo in moveit2/moveit2.repos $(f="moveit2/moveit2_$ROS_DISTRO.repos"; test -r $f && echo $f); do vcs import < "$repo"; done
+if [ -d "./src/motion_planning" ]; then
+	cd ./src/motion_planning
+	for repo in moveit2/moveit2.repos $(f="moveit2/moveit2_rolling.repos"; test -r $f && echo $f); do vcs import < "$repo"; done
+else
+	echo "Ignoring motion planning dependencies"
+fi
 
 # install rosdep dependencies
 cd ..
-rosdep install --from-paths . --ignore-src --rosdistro iron -r -y
+rosdep update
+rosdep install --from-paths . --ignore-src --rosdistro $ROS_DISTRO -r -y
 cd ..
 
 # build the workspace
