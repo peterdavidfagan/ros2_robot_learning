98051f0bc8721ee94f27a898f1e774ce9d6a96a0
peterdavidfagan <peterdavidfagan@gmail.com>
2024-04-01 10:10:47 +0100

    move libfranka install to workspace script

--- ros_workspace_setup.sh
+++ ros_workspace_setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-# update submodules
-git submodule sync
-git submodule update --init --recursive
+# install libfranka
+sudo apt update && apt upgrade -y
+sudo apt remove "*libfranka*" -y
+sudo apt install -y  build-essential cmake git libpoco-dev libeigen3-dev
+cd libfranka 
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF ..
+cmake --build .
+cpack -G DEB
+sudo dpkg -i libfranka*.deb
+
+# source iron installation and install middleware
+source /opt/ros/iron/setup.bash
+sudo apt install ros-$ROS_DISTRO-rmw-cyclonedds-cpp -y
 
 # install moveit dependencies
 cd ./src/motion_planning
@@ -10,10 +22,9 @@ for repo in moveit2/moveit2.repos $(f="moveit2/moveit2_$ROS_DISTRO.repos"; test 
 
 # install rosdep dependencies
 cd ..
-rosdep install --from-paths . --ignore-src --rosdistro rolling -r -y
+rosdep install --from-paths . --ignore-src --rosdistro iron -r -y
 cd ..
 
 # build the workspace
-source /opt/ros/rolling/setup.bash
-colcon build --mixin release
+colcon build --packages-ignore libfranka libfranka-common
 source ./install/setup.bash
