01eb7af7c15aa1ad69c03a09f84ebb8a219eccc0
peterdavidfagan <peterdavidfagan@gmail.com>
2024-04-15 11:21:54 +0100

    fix namespacing

--- src/tutorials/control/launch/franka_robotiq.launch.py
+++ src/tutorials/control/launch/franka_robotiq.launch.py
@@ -128,7 +128,7 @@ def generate_launch_description():
         name='joint_state_publisher',
         output='screen',
         parameters=[
-                {'source_list': ['panda_state_broadcaster/joint_states', 'robotiq_state_broadcaster/joint_states'],
+                {'source_list': ['/panda/panda_state_broadcaster/joint_states', '/robotiq/robotiq_state_broadcaster/joint_states'],
                  'rate': 30}],
     )
 
