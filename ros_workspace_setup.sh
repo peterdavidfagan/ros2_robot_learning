2f316cc23ced70ccb0d55aae0947b0d73427ab99
peterdavidfagan <peterdavidfagan@gmail.com>
2024-04-19 16:57:01 +0100

    start adding ros driver for ur

--- ros_workspace_setup.sh
+++ ros_workspace_setup.sh
@@ -3,19 +3,7 @@ set -e
 
 # install libfranka
 sudo apt update && apt upgrade -y || true
-sudo apt remove "*libfranka*" -y || true
 sudo apt install -y  build-essential cmake git libpoco-dev libeigen3-dev
-cd libfranka 
-if [ -d "./build" ]; then
-	rm -rf build
-fi
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF ..
-cmake --build .
-cpack -G DEB
-sudo dpkg -i libfranka*.deb
-cd ../..
 
 # source humble installation and install middleware
 source /opt/ros/humble/setup.bash
