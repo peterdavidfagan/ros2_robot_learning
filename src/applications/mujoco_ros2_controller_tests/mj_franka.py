d817890db62132303013b6c2ba7f1e53129f9eb9
peterdavidfagan <peterdavidfagan@gmail.com>
2024-05-20 14:42:40 +0100

    update modules + application code

--- src/applications/mujoco_ros2_controller_tests/mj_franka.py
+++ src/applications/mujoco_ros2_controller_tests/mj_franka.py
@@ -8,7 +8,6 @@ import mujoco_ros
 from mujoco_ros.franka_env import FrankaEnv
 
 model_filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'models', 'franka_controller_test.mjb')
-print(model_filepath)
 
 if __name__=="__main__":
     m = mujoco.MjModel.from_binary_path(model_filepath)
@@ -24,13 +23,13 @@ if __name__=="__main__":
             m, 
             d, 
             command_interface="effort",
-            control_steps=10, 
+            control_steps=5, 
             control_timer_freq=1e-2,
             )
 
         # run interactive viewer application
         while viewer.is_running():
-            time.sleep(0.01)
+            time.sleep(0.05)
             env.is_syncing = True
             viewer.sync()
             env.is_syncing = False 
